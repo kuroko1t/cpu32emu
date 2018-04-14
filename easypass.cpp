1f21022c4ac156c533b0135e81eaadfb4fd40c9f
kurosawa <kurosawa.yk@gmail.com>
2018-04-14 16:10:54 +0000

    add setup.sh(llvm functionname)

--- easypass.cpp
+++ easypass.cpp
@@ -1,26 +1,28 @@
+
 #include <cstdio>
 #include <llvm/Pass.h>
+#include <llvm/Support/raw_ostream.h>
 #include <llvm/IR/Function.h>
 
-class EasyPass : public FunctionPass {
+class EasyPass : public llvm::FunctionPass {
 public:
-  satic char ID;
+  static char ID;
   EasyPass() : FunctionPass(ID){}
   ~EasyPass(){}
-  virtual bool runOnFunction(Function &F);
+  virtual bool runOnFunction(llvm::Function &F);
   virtual void getAnalysisUsage(llvm::AnalysisUsage &AU) const;
 };
 
 
-bool EasyPass::runOnFunction(Function &F) {
-  llvm::errs() << "Funtion name" << F.getName().str() << "\n";
+bool EasyPass::runOnFunction(llvm::Function &F) {
+  llvm::errs() << "Funtion name : " << F.getName().str() << "\n";
   return false;
-};
+}
 
 void EasyPass::getAnalysisUsage(llvm::AnalysisUsage &AU) const{
   // not change
   AU.setPreservesAll();
-};
+}
 
 char EasyPass::ID = 0;
-static RegisterPass<EasyPass> X("easypass", "easy pass:only print functon name", false, false);
+static llvm::RegisterPass<EasyPass> X("easypass", "easy pass:only print functon name", false, false);
---
+++ setup.sh
@@ -0,0 +1,6 @@
+# make llvm ir
+clang --std=c++11  -emit-llvm -S -o main.ll main.cpp
+
+g++ -g easypass.cpp `llvm-config-3.8 --cxxflags --ldflags --libs` -c  -o easypass.o
+g++ -g -shared -o easypass.so easypass.o
+opt -load ./easypass.so -easypass main.ll
